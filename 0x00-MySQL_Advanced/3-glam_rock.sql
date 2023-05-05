ead72660d995d9f9ac804ea93668bf9d67bbc08a
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-05-05 04:07:33 +0300

    bands with Glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- lists all bands with Glam rock as their main style
+-- lists all bands with Glam rock as their main style.
 SELECT band_name, (IFNULL(split, '2022') - formed) AS lifspan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%' GROUP BY band_name , lifspan
