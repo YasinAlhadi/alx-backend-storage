3503133fe7491ba50b996a148eb010fb81b75f5f
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-05-07 16:01:48 +0300

    bands with Glam rock as their main style

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,6 +2,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     country VARCHAR(2) NOT NULL DEFAULT 'US' check (country in ('US', 'CO', 'TN'))
 );
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style.
-SELECT band_name, (IFNULL(split, '2022') - formed) AS lifspan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifspan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%' GROUP BY band_name , lifspan
 ORDER BY lifspan DESC;
