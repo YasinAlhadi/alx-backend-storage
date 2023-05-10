5f76c66a371f21d8ed4919ba749b451a0bcd1402
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-05-11 02:42:55 +0300

    recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
 
 
 class Cache:
@@ -18,3 +19,19 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Method that takes a key string argument and an optional Callable argument
+            and returns the converted data """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Method that will automatically parametrize Cache.get """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ Method that will automatically parametrize Cache.get """
+        return self.get(key, int)
